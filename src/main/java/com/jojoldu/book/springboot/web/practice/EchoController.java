501a9025c8619d75c4fae237f949878133c4387e
olivia <qwert45640@gmail.com>
2026-04-27 16:56:10 +0900

    [Day01] feat(practice): /echo/{message} 엔드포인트 추가

---
+++ src/main/java/com/jojoldu/book/springboot/web/practice/EchoController.java
@@ -0,0 +1,20 @@
+package com.jojoldu.book.springboot.web.practice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EchoController {
+
+    // TODO: GET /echo/{message} 매핑
+    @GetMapping("/echo/{message}")
+    public String echo(/* TODO: @PathVariable 적용 */ @PathVariable String message) {
+
+        // TODO: message를 거꾸로 뒤집어 반환
+        //       (힌트: new StringBuilder(message).reverse().toString())
+        return new StringBuilder(message).reverse().toString();
+
+    }
+
+}
